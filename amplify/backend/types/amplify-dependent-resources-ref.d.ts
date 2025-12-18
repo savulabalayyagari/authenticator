9dbcb0fd8676b73944bfc1070bc2400df098d002
savulabalayyagari <savulabalayyagari@uiowa.edu>
2025-12-17 20:18:56 -0600

    Add Amplify auth (Cognito) and fix setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "authenticatorvite2e29876a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
